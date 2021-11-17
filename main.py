10b4028718a834cb0f7da3572e5b559dfe42f3aa
Yaraslau Borzdy <92574019+Frameholder@users.noreply.github.com>
2021-11-17 20:45:11 +0100

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-
-per_cent={'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-a=(list(per_cent.values()))
-money=int(input("Введите сумму вашего депозита, USD "))/100
-deposit=(list(map((money).__mul__, a))) #функцию "mul" еще не изучали, однако подсказку нашел на https://question-it.com/questions/86250/kak-mne-umnozhit-kazhdyj-element-v-spiske-na-chislo
-print(deposit)
-print('Максимальная сумма, которую вы можете заработать',max(deposit), 'USD')
-
